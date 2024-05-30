e6dcf86c37d8f9e391855ebefcc41d736f0101bd
Firdavs Hoshimov <firdavs.the.best@mail.ru>
2024-05-30 21:28:43 +0500

    Implement presentation layer

--- app/src/main/java/com/firdavs/android/composition/domain/entity/GameResult.kt
+++ app/src/main/java/com/firdavs/android/composition/domain/entity/GameResult.kt
@@ -1,8 +1,12 @@
 package com.firdavs.android.composition.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class GameResult(
-    val winner: Boolean,
-    val countOfRightAnswers: Int, // количество правильных ответов
-    val countOfQuestions: Int,    // общее количество вопросов
-    val gameSettings: GameSettings
-)
+    var winner: Boolean,
+    var countOfRightAnswers: Int, // количество правильных ответов
+    var countOfQuestions: Int,    // общее количество вопросов
+    var gameSettings: GameSettings,
+) : Parcelable
---
+++ app/src/main/java/com/firdavs/android/composition/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,62 @@
+package com.firdavs.android.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.firdavs.android.composition.R
+import com.firdavs.android.composition.databinding.FragmentChooseLevelBinding
+import com.firdavs.android.composition.domain.entity.Level
+
+
+class ChooseLevelFragment : Fragment() {
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding = null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            buttonLevelTest.setOnClickListener {
+                launchGameFragment(Level.TEST)
+            }
+            buttonLevelEasy.setOnClickListener {
+                launchGameFragment(Level.EASY)
+            }
+            buttonLevelNormal.setOnClickListener {
+                launchGameFragment(Level.NORMAL)
+            }
+            buttonLevelHard.setOnClickListener {
+                launchGameFragment(Level.HARD)
+            }
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun launchGameFragment(level: Level){
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFragment.newInstance(level))
+            .addToBackStack(GameFragment.NAME)
+            .commit()
+    }
+
+    companion object{
+        const val NAME = "ChooseLevelFragment"
+        fun newInstance(): ChooseLevelFragment{
+            return ChooseLevelFragment()
+        }
+    }
+}
