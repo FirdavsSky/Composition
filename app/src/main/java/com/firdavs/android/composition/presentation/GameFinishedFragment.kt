a2f10aeb8501f853216bb2453b821b78b3cd153f
Firdavs Hoshimov <firdavs.the.best@mail.ru>
2024-06-01 00:23:59 +0500

    Finish app

--- app/src/main/java/com/firdavs/android/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/firdavs/android/composition/presentation/GameFinishedFragment.kt
@@ -36,48 +36,9 @@ class GameFinishedFragment : Fragment() {
         binding.buttonRetry.setOnClickListener {
             retryGame()
         }
-        bindViews()
+        binding.gameResult = args.gameResult
     }
 
-    private fun bindViews(){
-        with(binding){
-            emojiResult.setImageResource(getSmileResId())
-            tvRequiredAnswers.text = String.format(
-                getString(R.string.required_score),
-                args.gameResult.gameSettings.minCountIfRightAnswer
-            )
-            tvScoreAnswers.text = String.format(
-                getString(R.string.score_answers),
-                args.gameResult.countOfRightAnswers
-            )
-            tvRequiredPercentage.text = String.format(
-                getString(R.string.required_percentage),
-                args.gameResult.gameSettings.minPercentOfRightAnswer
-            )
-            tvRequiredPercentage.text = String.format(
-                getString(R.string.score_percentage),
-                getPercentOfRightAnswer()
-            )
-        }
-    }
-
-
-
-    private fun getSmileResId() : Int{
-        return if (args.gameResult.winner){
-            R.drawable.ic_smile
-        }else{
-            R.drawable.ic_sad
-        }
-    }
-
-    private fun getPercentOfRightAnswer() = with(args.gameResult){
-        if (countOfQuestions == 0){
-            0
-        }else{
-            ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
-        }
-    }
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -88,7 +49,5 @@ class GameFinishedFragment : Fragment() {
     private fun retryGame(){
         findNavController().popBackStack()
     }
-
-
 
 }
